a190998f0a5676703da6486249048a06b8347c2d
lasha-darchiashvili <ldarc17@freeuni.edu.ge>
2023-05-23 04:32:02 +0400

    FEAT: table created, retrive single and multiple contacts done

--- app.js
+++ app.js
@@ -28,7 +28,53 @@ app.get("/createDatabase", (req, res) => {
 
       console.log("Using Database");
 
+      const sql = `CREATE TABLE IF NOT EXISTS contacts (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        name VARCHAR(255) NOT NULL,
+                        email VARCHAR(255) NOT NULL,
+                        phone VARCHAR(20) NOT NULL
+                    )`;
+
+      db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+      });
+
       return res.send(`Created and Using`);
+    });
+  });
+});
+
+app.get("/contacts", (req, res) => {
+  const query = "SELECT * FROM contacts";
+  db.query(query, (err, results) => {
+    if (err) {
+      console.log("Error");
+      return res.status(500).json({ error: err.sqlMessage });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        contacts: results,
+      },
+    });
+  });
+});
+
+app.get("/contacts/:id", (req, res) => {
+  const query = `SELECT * FROM contacts WHERE id = ${req.params.id}`;
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log("Error fetching data");
+      return res.status(500).json({ error: err.sqlMessage });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        contact: result,
+      },
     });
   });
 });
