b7ffd7863e282782b2221e6d5d7ff0b1f0361589
lasha-darchiashvili <ldarc17@freeuni.edu.ge>
2023-05-23 06:00:36 +0400

    FEAT: insert and update contact added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "lasha",
+  database: "contact_management",
 });
 
 db.connect((err) => {
@@ -14,35 +15,29 @@ db.connect((err) => {
   console.log("Connected");
 });
 
+app.use(express.json());
+
 app.get("/createDatabase", (req, res) => {
   let createQuery = "CREATE DATABASE IF NOT EXISTS contact_management";
 
   db.query(createQuery, (err, result) => {
     if (err) throw err;
-
     console.log("Database Created or exists");
 
-    let useQuery = `USE contact_management`;
-    db.query(useQuery, (error) => {
-      if (error) throw error;
-
-      console.log("Using Database");
-
-      const sql = `CREATE TABLE IF NOT EXISTS contacts (
-                        id INT AUTO_INCREMENT PRIMARY KEY,
-                        name VARCHAR(255) NOT NULL,
-                        email VARCHAR(255) NOT NULL,
-                        phone VARCHAR(20) NOT NULL
-                    )`;
-
-      db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-      });
-
-      return res.send(`Created and Using`);
-    });
+    return res.send(`Created and Using`);
   });
+});
+
+const sql = `CREATE TABLE IF NOT EXISTS contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL
+)`;
+
+db.query(sql, (err, result) => {
+  if (err) throw err;
+  console.log(result);
 });
 
 app.get("/contacts", (req, res) => {
@@ -74,6 +69,56 @@ app.get("/contacts/:id", (req, res) => {
       status: "success",
       data: {
         contact: result,
+      },
+    });
+  });
+});
+
+app.post("/contacts", (req, res) => {
+  const { name, email, phone } = req.body;
+
+  const query = "INSERT INTO contacts (name, email, phone) VALUES (?, ?, ?)";
+  db.query(query, [name, email, phone], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: err.sqlMessage });
+    }
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        id: result.insertId,
+        name: name,
+        email: email,
+        phone: phone,
+      },
+    });
+  });
+});
+
+app.put("/contacts/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone } = req.body;
+
+  const query =
+    "UPDATE contacts SET name = ?, email = ?, phone = ? WHERE id = ?";
+  db.query(query, [name, email, phone, id], (err, result) => {
+    if (err) {
+      console.error("Error");
+      return res.status(500).json({ error: err.sqlMessage });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "ID not found" });
+    }
+
+    res.json({
+      status: "success",
+      data: {
+        id: id,
+        name: name,
+        email: email,
+        phone: phone,
       },
     });
   });
